02a95bd29bdcd15975a18e2b8383a09c30ac7fb9
MoathElmarmori <moath222elmarmori@gmail.com>
2022-10-30 14:24:55 +0200

    third commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const cors = require("cors");
 
 const graphqlSchema = require("./schema/graphqlSchema");
